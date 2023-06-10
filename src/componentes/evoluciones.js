d9f19ae7ccf4420f16b51ad10b29b2a284a5505b
carlioso3000 <carlioso3006@gmail.com>
2023-06-10 18:00:54 -0400

    componente evoluciones creado, funcion fetch a las evoluciones creada

---
+++ src/componentes/evoluciones.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+function Evoluciones({nombre1, nombre2, nombre3, imagen}) {
+
+    return (
+        <ul>
+            <li>{nombre1}</li>
+            <li>{nombre2}</li>
+            <li>{nombre3}</li>
+        </ul>
+    )
+}
+
+export default Evoluciones;
