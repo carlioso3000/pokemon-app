83df270de62ce9df659b58626fd17212d0ef9d39
carlioso3000 <carlioso3006@gmail.com>
2023-06-10 18:02:38 -0400

    funcion fetch para las evoluciones creada

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Buscador from './componentes/buscador'
 import Boton from './componentes/Boton'
 import PokemonLogo from './imagenes/pokemon-logo.png';
 import React, {useState, useEffect} from 'react';
+import Evoluciones from './componentes/evoluciones';
 
 
 function App() {
@@ -23,6 +24,50 @@ async function getPokemon (name) {
     imagen: data.sprites.other["official-artwork"].front_default, 
     identificador: data.id})
 };
+
+// ////////////////////////////////////////////////// funcion para obtener cadena evolutiva ///////////////////////////////////////////////////
+
+const [pokemonEvolution, setpokemonEvolution] = useState({evolucion1: '', evolucion2:"", evolucion3:"", evoImagen: "", identificador: 1});
+
+useEffect (()=>{
+  getEvolutions(pokemonEvolution.identificador)
+}, [])
+
+async function getEvolutions(id) {
+  const res = await fetch(`https://pokeapi.co/api/v2/evolution-chain/${id}/`)
+  const evolutionData = await res.json()
+
+  setpokemonEvolution({
+    evolucion1: evolutionData.chain.species.name,
+    evolucion2: evolutionData.chain.evolves_to[0].species.name,
+    evolucion3: evolutionData.chain.evolves_to[0].evolves_to[0].species.name,
+    evoImagen: evolutionData.chain.species.name
+  })
+};
+
+/*
+async function evoluciones(name) {
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${name}/`)
+  const speciesData = await res.json()
+
+  const evolutionChain = await fetch(speciesData.evolution_chain.url)
+  const evolutionData = await evolutionChain.json()
+  const evolution = []
+
+  function recorrerEvoluciones(chain) {
+    evolution.push(chain.species.name)
+
+    if (chain.evolves_to.length > 0 ) {
+      recorrerEvoluciones(chain.evolves_to[0])
+    }
+  }
+
+  recorrerEvoluciones(evolutionData.chain)
+  return evolution;
+}
+*/
+//evoluciones('rattata').then(evolucionesPokemon => console.log(evolucionesPokemon))
+
 // creamos la logica para los botones de prev y next
 
 function obtenerPokemonAnterior () {
@@ -63,6 +108,10 @@ const pokemonApp = []
             siguientePokemon={obtenerPokemonSiguiente} 
             />
         </div>
+
+        <Evoluciones 
+          nombre1= {pokemonEvolution.evolucion1} nombre2={pokemonEvolution.evolucion2} nombre3={pokemonEvolution.evolucion3}
+        />
       </div> 
     </div>
   );
